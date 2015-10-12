ce097fb56a1638dd35b8d7e67a066d9e7df52c69
gries <christoph@rosse.at>
2015-10-12 20:41:54 +0200

    Add position

--- spec/PositionSpec.php
+++ spec/PositionSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\gries\Minecraft\Builder;
 
+use gries\Minecraft\Builder\Position;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -9,8 +10,61 @@ class PositionSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
+        $this->beConstructedWith(2, 3, 2);
         $this->shouldHaveType('gries\Minecraft\Builder\Position');
     }
 
+    function it_should_render_as_formatted_string()
+    {
+        $this->beConstructedWith(2, 3, 2);
 
+        $this->format()->shouldBe('2 3 2');
+    }
+
+    function it_stores_coordinations_correctly()
+    {
+        $this->beConstructedWith(2, 3, 2);
+        $this->x()->shouldBe(2);
+        $this->y()->shouldBe(3);
+        $this->z()->shouldBe(2);
+    }
+
+    function adding_another_position_creates_a_new_position()
+    {
+        $this->beConstructedWith(2, 3, 2);
+        $this->add(new Position(1, -2, 0));
+
+        $this->shouldMatchPosition(new Position(3, 1, 2));
+    }
+
+    function it_lets_us_modify_x()
+    {
+        $this->beConstructedWith(2, 3, 2);
+        $this->modifyX(-1)->shouldMatchPosition(new Position(1, 3, 2));
+    }
+
+    function it_lets_us_modify_y()
+    {
+        $this->beConstructedWith(2, 3, 2);
+        $this->modifyY(-1)->shouldMatchPosition(new Position(2, 2, 2));
+    }
+
+    function it_lets_us_modify_z()
+    {
+        $this->beConstructedWith(2, 3, 2);
+        $this->modifyZ(-1)->shouldMatchPosition(new Position(2, 3, 1));
+    }
+
+    public function getMatchers()
+    {
+        return [
+            'matchPosition' => function(Position $position1, Position $position2) {
+                
+                return
+                    $position1->x() === $position2->x() &&
+                    $position1->y() === $position2->y() &&
+                    $position1->z() === $position2->z();
+            }
+        ];
+    }
 }
--- src/Position.php
+++ src/Position.php
@@ -2,6 +2,119 @@
 
 namespace gries\Minecraft\Builder;
 
+
+/**
+ * Class Position
+ *
+ * @package gries\Minecraft\Builder
+ */
 class Position
 {
+    /**
+     * @var mixed
+     */
+    protected $x;
+
+    /**
+     * @var mixed
+     */
+    protected $y;
+
+    /**
+     * @var mixed
+     */
+    protected $z;
+
+    /**
+     * @param $x
+     * @param $y
+     * @param $z
+     */
+    public function __construct($x, $y, $z)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->z = $z;
+    }
+
+
+    /**
+     * Get the mincraft-command Formatted representation of this position.
+     */
+    public function format()
+    {
+        return sprintf('%s %s %s', $this->x(), $this->y(), $this->z());
+    }
+
+    /**
+     * Get a new Position with a modified X-Coordinate.
+     *
+     * @param $xModifier
+     *
+     * @return Position
+     */
+    public function modifyX($xModifier)
+    {
+        $newX = $this->x() + $xModifier;
+
+        return new Position($newX, $this->y(), $this->z());
+    }
+
+    /**
+     * Get a new Position with a modified Y-Coordinate.
+     *
+     * @param $yModifier
+     *
+     * @return Position
+     */
+    public function modifyY($yModifier)
+    {
+        $newY = $this->y() + $yModifier;
+
+        return new Position($this->x(), $newY, $this->z());
+    }
+
+    /**
+     * Get a new Position with a modified Z-Coordinate.
+     *
+     * @param $zModifier
+     *
+     * @return Position
+     */
+    public function modifyZ($zModifier)
+    {
+        $newZ = $this->z() + $zModifier;
+
+        return new Position($this->x(), $this->y(), $newZ);
+    }
+
+    /**
+     * Get the X-CoordinateValue.
+     *
+     * @return mixed
+     */
+    public function x()
+    {
+        return $this->x;
+    }
+
+    /**
+     * Get the Y-CoordinateValue.
+     *
+     * @return mixed
+     */
+    public function y()
+    {
+        return $this->y;
+    }
+
+    /**
+     * Get the Z-CoordinateValue.
+     *
+     * @return mixed
+     */
+    public function z()
+    {
+        return $this->z;
+    }
 }
